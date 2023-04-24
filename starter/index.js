b9f70869718e5e18bc64da43bcea6432e01b8708
Deep Kapadia <deepkapadia678@gmail.com>
2023-04-24 17:21:31 +0530

    returning values from asynchronous functions

--- starter/index.js
+++ starter/index.js
@@ -21,7 +21,37 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/doggg.txt`)
+const getDogOic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/doog.txt`);
+    console.log(`Breed : ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('File written');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return '2 : Ready';
+};
+console.log('1 : will get dog pics');
+/*getDogOic()
+  .then((x) => {
+    console.log(x);
+    console.log('3 : Done');
+  })
+  .catch((err) => {
+    console.log('ERROR');
+  });
+  */
+
+/*
+readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed : ${data}`);
     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
@@ -39,3 +69,4 @@ readFilePro(`${__dirname}/doggg.txt`)
   .catch((err) => {
     console.log(err);
   });
+  */
